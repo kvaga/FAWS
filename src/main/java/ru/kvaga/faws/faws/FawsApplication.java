e49100075941207f0e40227d7ccbe830c138fbd5
kvaga <32394407+kvaga@users.noreply.github.com>
2024-09-26 20:56:50 +0300

    Initial

---
+++ src/main/java/ru/kvaga/faws/faws/FawsApplication.java
@@ -0,0 +1,14 @@
+package ru.kvaga.faws.faws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FawsApplication {
+
+	public static void main(String[] args) {
+		System.out.println("QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ");
+		SpringApplication.run(FawsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kvaga/faws/faws/FawsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kvaga.faws.faws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FawsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
